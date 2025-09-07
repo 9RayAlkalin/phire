baa0c2a7fa4039fe5f5cd3e28afd0e5384bb6e7a
HLMC <hl2278535805@outlook.com>
2025-09-07 20:21:57 +0800

    refactor: decode video with video-rs

--- phire/src/core/video.rs
+++ phire/src/core/video.rs
@@ -3,39 +3,37 @@ use crate::ext::{source_of_image, ScaleType};
 use anyhow::{Ok, Result};
 use macroquad::prelude::*;
 use miniquad::{Texture, TextureFormat, TextureParams, TextureWrap};
-use prpr_avc::AVPixelFormat;
+use video_rs::{Decoder, DecoderBuilder};
 use std::{cell::RefCell, io::Write};
 use tempfile::NamedTempFile;
 
 thread_local! {
-    static VIDEO_BUFFERS: RefCell<[Vec<u8>; 3]> = RefCell::default();
+    static VIDEO_BUFFER: RefCell<Vec<u8>> = RefCell::default();
 }
 
 pub struct Video {
-    video: prpr_avc::Video,
+    decoder: Decoder,
     pub video_file: NamedTempFile,
 
     material: Material,
-    tex_y: Texture2D,
-    tex_u: Texture2D,
-    tex_v: Texture2D,
+    tex_rgb: Texture2D,
 
     start_time: f32,
     scale_type: ScaleType,
     alpha: Anim<f32>,
     dim: Anim<f32>,
-    frame_delta: f64,
+    frame_delta: f32,
     pub next_frame: usize,
     pub ended: bool,
 }
 
-fn new_tex(w: u32, h: u32) -> Texture2D {
+fn new_tex_rgb(w: u32, h: u32) -> Texture2D {
     Texture2D::from_miniquad_texture(Texture::new_render_texture(
         unsafe { get_internal_gl() }.quad_context,
         TextureParams {
             width: w,
             height: h,
-            format: TextureFormat::Alpha,
+            format: TextureFormat::RGB8,
             filter: FilterMode::Linear,
             wrap: TextureWrap::Clamp,
         },
@@ -44,14 +42,15 @@ fn new_tex(w: u32, h: u32) -> Texture2D {
 
 impl Video {
     pub fn new(data: Vec<u8>, start_time: f32, scale_type: ScaleType, alpha: Anim<f32>, dim: Anim<f32>) -> Result<Self> {
+        video_rs::init().unwrap();
         let mut video_file = NamedTempFile::new()?;
         video_file.write_all(&data)?;
         drop(data);
-        let video = prpr_avc::Video::open(video_file.path().as_os_str().to_str().unwrap(), AVPixelFormat::YUV420P)?;
-        let frame_delta = video.frame_rate().to_f64_inv();
-        let format = video.stream_format();
-        let w = format.width as u32;
-        let h = format.height as u32;
+        let decoder = DecoderBuilder::new(video_file.path()).build()?;
+        let frame_delta = 1. / decoder.frame_rate();
+        let size = decoder.size();
+        let w = size.0;
+        let h = size.1;
 
         let material = load_material(
             shader::VERTEX,
@@ -59,24 +58,18 @@ impl Video {
             MaterialParams {
                 pipeline_params: PipelineParams::default(),
                 uniforms: Vec::new(),
-                textures: vec!["tex_y".to_owned(), "tex_u".to_owned(), "tex_v".to_owned()],
+                textures: vec!["tex_rgb".to_owned()],
             },
         )?;
-        let tex_y = new_tex(w, h);
-        let tex_u = new_tex(w / 2, h / 2);
-        let tex_v = new_tex(w / 2, h / 2);
-        material.set_texture("tex_y", tex_y);
-        material.set_texture("tex_u", tex_u);
-        material.set_texture("tex_v", tex_v);
+        let tex_rgb = new_tex_rgb(w, h);
+        material.set_texture("tex_rgb", tex_rgb);
 
         Ok(Self {
-            video,
+            decoder,
             video_file,
 
             material,
-            tex_y,
-            tex_u,
-            tex_v,
+            tex_rgb,
 
             start_time,
             scale_type,
@@ -94,32 +87,28 @@ impl Video {
         }
         self.alpha.set_time(t);
         self.dim.set_time(t);
-        let that_frame = ((t - self.start_time) as f64 / self.frame_delta) as usize;
+        let that_frame = ((t - self.start_time) / self.frame_delta) as usize;
         if self.next_frame <= that_frame {
-            VIDEO_BUFFERS.with(|it| {
+            VIDEO_BUFFER.with(|it| {
                 let mut buf = it.borrow_mut();
                 while self.next_frame <= that_frame {
-                    buf[0].clear();
-                    buf[1].clear();
-                    buf[2].clear();
-                    if self
-                        .video
-                        .with_frame(|frame| {
-                            buf[0].extend_from_slice(frame.data(0));
-                            buf[1].extend_from_slice(frame.data_half(1));
-                            buf[2].extend_from_slice(frame.data_half(2));
-                        })
-                        .is_none()
-                    {
+                    if let ::std::result::Result::Ok((_, frame)) = self.decoder.decode() {
+                        if self.next_frame < that_frame {
+                            self.next_frame += 1;
+                            continue;
+                        }
+                        let rgb = frame.as_slice().unwrap();
+                        buf.clear();
+                        buf.extend_from_slice(rgb);
+                    } else {
                         self.ended = true;
                         return;
                     }
                     self.next_frame += 1;
                 }
+
                 let ctx = unsafe { get_internal_gl() }.quad_context;
-                self.tex_y.raw_miniquad_texture_handle().update(ctx, &buf[0]);
-                self.tex_u.raw_miniquad_texture_handle().update(ctx, &buf[1]);
-                self.tex_v.raw_miniquad_texture_handle().update(ctx, &buf[2]);
+                self.tex_rgb.raw_miniquad_texture_handle().update(ctx, &buf);
             });
         }
         Ok(())
@@ -132,7 +121,7 @@ impl Video {
         gl_use_material(self.material);
         let top = 1. / res.aspect_ratio;
         let r = Rect::new(-1., -top, 2., top * 2.);
-        let s = source_of_image(&self.tex_y, r, self.scale_type).unwrap_or_else(|| Rect::new(0., 0., 1., 1.));
+        let s = source_of_image(&self.tex_rgb, r, self.scale_type).unwrap_or_else(|| Rect::new(0., 0., 1., 1.));
         let dim = 1. - self.dim.now();
         let color = Color::new(dim, dim, dim, self.alpha.now_opt().unwrap_or(1.));
         let vertices = [
@@ -150,7 +139,7 @@ impl Video {
     pub fn reset(&mut self) -> Result<()> {
         self.next_frame = 0;
         self.ended = false;
-        self.video = prpr_avc::Video::open(self.video_file.path().as_os_str().to_str().unwrap(), AVPixelFormat::YUV420P)?;
+        self.decoder.seek_to_start()?;
         Ok(())
     }
 }
@@ -179,23 +168,10 @@ precision lowp float;
 varying lowp vec4 color;
 varying lowp vec2 uv;
 
-uniform sampler2D tex_y;
-uniform sampler2D tex_u;
-uniform sampler2D tex_v;
+uniform sampler2D tex_rgb;
 
 void main() {
-    vec3 yuv = vec3(
-        texture2D(tex_y, uv).a,
-        texture2D(tex_u, uv).a - 0.5,
-        texture2D(tex_v, uv).a - 0.5
-    );
-    yuv.x = 1.1643 * (yuv.x - 0.0625);
-    mat3 color_matrix = mat3(
-        vec3(1.0,   0.0,     1.402),
-        vec3(1.0,  -0.344,  -0.714),
-        vec3(1.0,   1.772,   0.0  )
-    );
-
-    gl_FragColor = vec4(yuv * color_matrix, 1.0) * color;
+    vec3 rgb = texture2D(tex_rgb, uv).rgb;
+    gl_FragColor = vec4(rgb, 1.0) * color;
 }"#;
 }
